0746162c7f27c7200687f2e179e4f545d65941b9
Coen de Lange <delange.coen@gmail.com>
2024-04-04 11:19:38 +0200

    split the data into test, train and validation

--- datasplit.py
+++ datasplit.py
@@ -12,16 +12,26 @@ ben = len(benign.index)
 '''We see that the amount of bening and malignant is relatively the same. Therefore,
  our validation set wil contain 8 bening samples and 8 malignant samples'''
 
-#pick 8 random samples from both sets. using random state for reproducability
+#pick 8 random samples from both sets for validation
 malignant_valdation = malignant.sample(n=8, random_state=1)
 benign_validation = benign.sample(n=8, random_state=1)
 
-#remove the validation samples from the train set
-malignant_train = malignant.drop(malignant_valdation.index).reset_index(drop=True)
-bening_train = benign.drop(benign_validation.index).reset_index(drop=True)
+#remove the validation samples from the test and train set
+malignant_testtrain = malignant.drop(malignant_valdation.index).reset_index(drop=True)
+benign_testtrain = benign.drop(benign_validation.index).reset_index(drop=True)
+
+#pick 8 random samples from both sets for testing
+malignant_test = malignant_testtrain.sample(n=8, random_state=1)
+benign_test = benign_testtrain.sample(n=8, random_state=1)
+
+#remove the train samples from the train set
+malignant_train = malignant_testtrain.drop(malignant_test.index).reset_index(drop=True)
+benign_train = benign_testtrain.drop(benign_test.index).reset_index(drop=True)
 
 #combining the dfs into workable sets and writing them to csv
 validation = pd.concat([malignant_valdation,benign_validation])
-train = pd.concat([malignant_train,bening_train])
+test = pd.concat([malignant_test,benign_test])
+train = pd.concat([malignant_train,benign_train])
 validation.to_csv('validation_data.csv', index=False)
+test.to_csv('test_data.csv', index=False)
 train.to_csv('train_data.csv', index=False)
